1f3ffcfbf3cc43d6249d616d65019a25c483da72
smritip <smriti@mit.edu>
2019-03-18 12:56:15 -0400

    Adding MMD creation

---
+++ mmd_creator.py
@@ -0,0 +1,40 @@
+## Smriti Pramanick
+## Using Dynamic Time Warping to Improve the Classical Music Production Workflow
+
+import xml.etree.ElementTree as ET
+from constants import *
+from random import *
+
+
+def make_mmd(timecodes, names, ratings, filename):
+	root = ET.Element("MediaMetaData")
+	ET.SubElement(root, "TakeName")
+	ET.SubElement(root, "TakeNotes")
+	marker_list = ET.SubElement(root, "MediaMarkerList")
+
+	for i in range(len(timecodes)):
+		marker = ET.SubElement(marker_list, "MediaMarker")
+		ET.SubElement(marker, "UID").text = get_uid()
+		ET.SubElement(marker, "TimeCode").text = timecodes[i]
+		ET.SubElement(marker, "Name").text = names[i]
+		ET.SubElement(marker, "Comment")
+		ET.SubElement(marker, "Rating").text = ratings[i]
+
+	tree = ET.ElementTree(root)
+	tree.write(filename)
+
+def get_uid():
+	uids = [str(randint(0, 255)) for i in range(16)]
+	uid = "-".join(uids)
+	prefix = "16-"
+	uid = prefix + uid
+	return uid
+
+def make_10_markers():  # every 5 seconds
+	timecodes = [str(fs_bso * (5 * i) * 23520) for i in range(10)]
+	names = [str(i) for i in range(10)]
+	ratings = ['2' for i in range(10)]
+	
+	make_mmd(timecodes, names, ratings, "10markers.mmd")
+
+make_10_markers()
